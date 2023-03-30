5ac0b149aaba3de7d60f55ef04d475fc1869049a
Eduardo Pandini <pandiniedu@gmail.com>
2023-03-30 12:46:52 -0300

    gogo

--- teste2.py
+++ teste2.py
@@ -1,85 +1,50 @@
-import random
 import time
+import random
 import matplotlib.pyplot as plt
 
-def heap_sort(arr):
-    def heapify(arr, n, i):
-        largest = i
-        l = 2 * i + 1
-        r = 2 * i + 2
-
-        if l < n and arr[largest] < arr[l]:
-            largest = l
-
-        if r < n and arr[largest] < arr[r]:
-            largest = r
-
-        if largest != i:
-            arr[i], arr[largest] = arr[largest], arr[i]
-            heapify(arr, n, largest)
-
-    n = len(arr)
-
-    for i in range(n // 2 - 1, -1, -1):
-        heapify(arr, n, i)
-
-    for i in range(n - 1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]
-        heapify(arr, i, 0)
-
-def linear_search(arr, x):
-    for i in range(len(arr)):
-        if arr[i] == x:
+def busca_linear(lista, item):
+    for i in range(len(lista)):
+        if lista[i] == item:
             return i
-    return -1
+    return None
 
-def binary_search(arr, x):
-    low = 0
-    high = len(arr) - 1
-    while low <= high:
-        mid = (low + high) // 2
-        if arr[mid] < x:
-            low = mid + 1
-        elif arr[mid] > x:
-            high = mid - 1
+def busca_binaria(lista, item):
+    baixo = 0
+    alto = len(lista) - 1
+    while baixo <= alto:
+        meio = (baixo + alto) // 2
+        if lista[meio] == item:
+            return meio
+        elif lista[meio] > item:
+            alto = meio - 1
         else:
-            return mid
-    return -1
+            baixo = meio + 1
+    return None
 
-# Gerando listas aleatórias de diferentes tamanhos
-sizes = [100, 1000, 10000, 100000, 1000000]
-arrays = []
-for size in sizes:
-    arrays.append(random.sample(range(size * 10), size))
+tempos_linear = []
+tempos_binaria = []
+tamanhos = [10**i for i in range(1, 6)]
 
-# Medindo o tempo de execução dos algoritmos para cada lista
-heap_sort_times = []
-linear_search_times = []
-binary_search_times = []
-for arr in arrays:
-    start = time.time()
-    heap_sort(arr)
-    end = time.time()
-    heap_sort_times.append(end - start)
+for tamanho in tamanhos:
+    lista = [random.randint(0, tamanho) for _ in range(tamanho)]
+    item = random.randint(0, tamanho)
 
-    x = random.randint(0, size * 10)
-    start = time.time()
-    linear_search(arr, x)
-    end = time.time()
-    linear_search_times.append(end - start)
+    inicio = time.time()
+    busca_linear(lista, item)
+    fim = time.time()
+    tempos_linear.append(fim - inicio)
 
-    arr.sort()
-    x = random.randint(0, size * 10)
-    start = time.time()
-    binary_search(arr, x)
-    end = time.time()
-    binary_search_times.append(end - start)
+    inicio = time.time()
+    busca_binaria(sorted(lista), item)
+    fim = time.time()
+    tempos_binaria.append(fim - inicio)
 
-# Plotando os resultados
-plt.plot(sizes, heap_sort_times, label='Heap Sort')
-plt.plot(sizes, linear_search_times, label='Busca Linear')
-plt.plot(sizes, binary_search_times, label='Busca Binária')
+plt.plot(tamanhos, tempos_linear, 'r', label='Busca Linear')
+plt.plot(tamanhos, tempos_binaria, 'b', label='Busca Binária')
+plt.xscale('log')
+plt.yscale('log')
 plt.xlabel('Tamanho da entrada')
 plt.ylabel('Tempo de execução (s)')
+plt.title('Comparação de Busca Linear e Busca Binária')
 plt.legend()
 plt.show()
